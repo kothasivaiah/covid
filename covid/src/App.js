8d1a3b275111213363b147dd11c14c020e822c42
kothasivaiah <kothasivaiah99@gmail.com>
2022-04-05 11:47:42 +0530

    added covied file

---
+++ covid/src/App.js
@@ -0,0 +1,45 @@
+import React,{useEffect,useState} from 'react'
+
+const App = () => {
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    fetch('https://api.covid19india.org/data.json').then(
+      res => res.json()
+    ).then(jsondata => setData(jsondata.statewise))
+  },[])
+  return (
+    <div>
+      <h1>INDIA COVID-19 Dash Board</h1>
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th>State</th>
+            <th>Confirmed</th>
+            <th>Recovered</th>
+            <th>Deaths</th>
+            <th>Active</th>
+            <th>LastUpdate</th>
+          </tr>
+        </thead>
+          <tbody>
+            {data.map((item,index) =>{
+              return(
+                <tr key={index}>
+                  <td>{item.state}</td>
+                  <td>{item.confirmed}</td>
+                  <td>{item.recovered}</td>
+                  <td>{item.deaths}</td>
+                  <td>{item.active}</td>
+                  <td>{item.lastupdatedtime}</td>
+                </tr>
+              )
+            })}
+
+          </tbody>
+        
+      </table>
+    </div>
+  )
+}
+
+export default App
